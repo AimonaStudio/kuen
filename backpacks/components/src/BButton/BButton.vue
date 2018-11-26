b7e2a7cee2f3c68690fa915aee7a562e90e24dd4
himself65 <himself6565@gmail.com>
2018-11-27 00:26:16 +0800

    Add BButton

--- backpacks/components/src/BButton/BButton.vue
+++ backpacks/components/src/BButton/BButton.vue
@@ -1,14 +1,60 @@
-<!-- todo -->
-<template>
-  <button />
-</template>
-
 <script>
+import Colorable from '../mixins/colorable'
+import Themeable from '../mixins/themeable'
+
 export default {
-  name: 'BButton'
+  name: 'BButton',
+
+  mixins: [Colorable, Themeable],
+
+  props: {
+    size: { type: String, default: 'medium' },
+    round: { type: Boolean, default: true },
+    disabled: { type: Boolean, default: false },
+    flat: { type: Boolean, default: false },
+    block: { type: Boolean, default: false },
+    large: { type: Boolean, default: false },
+    small: { type: Boolean, default: false }
+  },
+
+  computed: {
+    classes () {
+      return {
+        ...this.themeClasses,
+        'b-button': true,
+        'b-button--round': this.round,
+        'b-button--disabled': this.disabled,
+        'b-button--flat': this.flat,
+        'b-button--small': this.small,
+        'b-button--large': this.large
+      }
+    },
+    styles () {
+      return {}
+    }
+  },
+
+  render () {
+    const data = {
+      class: {
+        ...this.classes
+      },
+      style: {
+        ...this.styles
+      }
+    }
+    this.setBackgroundColor(this.color, data)
+    return (<button
+      disabled={this.disable}
+      class={data.class}
+      style={data.style}
+    >
+      <div class="b-button--body">
+        {this.$slots.default}
+      </div>
+    </button>)
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="stylus" src="./BButton.styl"></style>
