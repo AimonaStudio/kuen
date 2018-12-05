69e99f03695082712a09d68e613cc9ba6e6e1baa
himself65 <himself6565@gmail.com>
2018-12-05 23:32:48 +0800

    ADD a new component: b-footer

---
+++ backpacks/components/src/BFooter/BFooter.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'BFooter',
+
+  computed: {
+    classes () {
+      return {
+        'b-footer': true
+      }
+    },
+    styles () {
+      return {}
+    }
+  },
+
+  render () {
+    const data = {
+      class: {
+        ...this.classes
+      },
+      style: {
+        ...this.styles
+      }
+    }
+    return (
+      <footer {...data}>
+        {this.$slots.default}
+      </footer>
+    )
+  }
+}
+</script>
+
+<style lang="stylus" src="./BFooter.styl"></style>
