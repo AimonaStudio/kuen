d70952cb18b9a6062227af32d8fa36a2ee00c343
himself65 <himself6565@gmail.com>
2018-12-07 23:05:37 +0800

    ADD BHeader

---
+++ backpacks/components/src/BHeader/BHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'BHeader',
+
+  computed: {
+    classes () {
+      return {
+        'b-header': true
+      }
+    },
+
+    styles () {
+      return {
+
+      }
+    }
+  },
+
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+</script>
+
+<style lang="stylus" src="./BHeader.styl"></style>
