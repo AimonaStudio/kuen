4618c07244778a2a3c9f92854ee28e28a84eaf62
himself65 <himself6565@gmail.com>
2018-12-10 22:29:09 +0800

    FIX color too lightly REMOVE @kuen/components on loader, whether use components is up to user

--- backpacks/loader/src/components/index.js
+++ backpacks/loader/src/components/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import Kuen from '@kuen/components'
 
 const initComponents = (conf) => {
   const { theme, config } = conf
   Vue.prototype.$kuenGetTheme = theme
   Vue.prototype.$kuenGetConfig = config
-  Vue.use(Kuen)
 }
 
 export { initComponents }
--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,12 @@
+// global
+import Vue from 'vue'
+// local
 import routerConf from './router'
 import App from './App.vue'
 import { startApp } from '../backpacks/loader/src'
+import Kuen from '../backpacks/components/src'
 
+Vue.use(Kuen)
 startApp({
   routerConf: routerConf,
   appView: App
