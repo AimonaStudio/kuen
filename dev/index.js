c54e36825d8e2e60ae242bf21955b0697f5ddf73
himself65 <himself6565@gmail.com>
2018-12-10 21:33:27 +0800

    FIX type FXI dependencies

--- dev/index.js
+++ dev/index.js
@@ -1,8 +1,8 @@
-import routeConf from './router'
+import routerConf from './router'
 import App from './App.vue'
 import { startApp } from '../backpacks/loader/src'
 
 startApp({
-  routerConf: routeConf,
+  routerConf: routerConf,
   appView: App
 })
