d8e9d252423fbe01d88b9f27b0ef8dd0101cb154
himself65 <himself6565@gmail.com>
2018-12-19 23:45:05 +0800

    FIX build error

--- backpacks/loader/vue.config.js
+++ backpacks/loader/vue.config.js
@@ -1,1 +1,17 @@
-module.exports = {}
+const autoprefixer = require('autoprefixer')
+const mqpacker = require('css-mqpacker')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer({
+            browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all']
+          }),
+          mqpacker()
+        ]
+      }
+    }
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,22 @@
-// var path = require('path')
+const autoprefixer = require('autoprefixer')
+const mqpacker = require('css-mqpacker')
 
 module.exports = {
   pages: {
     index: {
       entry: './dev/index.js'
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer({
+            browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all']
+          }),
+          mqpacker()
+        ]
+      }
     }
   }
 }
