2d60b198cdb3458f5970db0dbea354fad2415226
himself65 <himself6565@gmail.com>
2018-12-24 22:44:10 +0800

    ADD webpack config: alias, extensions

--- backpacks/components/vue.config.js
+++ backpacks/components/vue.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
 const autoprefixer = require('autoprefixer')
 const mqpacker = require('css-mqpacker')
 
 module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('~@', path.resolve(__dirname, 'node_modules'))
+    config.resolve.extensions.merge(['.styl'])
+  },
   css: {
     loaderOptions: {
       postcss: {
